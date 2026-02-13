095c0cad91d592cd29a81bd2e463c048ea0b6d61
Bruno mendes <162513613+BrunoMendesMorais@users.noreply.github.com>
2026-02-13 19:56:22 -0300

    Organização do código

--- main.c
+++ main.c
@@ -1,42 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int i;
-	unsigned int numeros[500];
-	unsigned int valor;
+void main(int argc, char *argv[]) {
+	int i,tempo;
+	unsigned int valor, numeros[500];
 	clock_t c,f;
-	int tempo;
 	
 	for(i=0;i<500;i++){
 		numeros[i] = rand() % 10;
  	}
  	
-	printf("\nQual valor deseja buscar?\n");
+	printf("Qual valor deseja buscar?\n");
 	scanf("%u",&valor);
 	
 	c = clock();
-	
 	for(i=0;i<500 && numeros[i]!=valor;i++){
-		numeros[i];
 		usleep(1000);
  	}
- 	
  	f = clock();
  	
  	tempo = f-c;
- 	
- 	if(numeros[i]!=valor){
- 		printf("numero nao encontrado");
-	 }
-	else
-		printf("O numero que pediu esta na posicao: %d", i);
+ 	numeros[i]!=valor ? printf("numero nao encontrado"):printf("O numero que pediu esta na posicao: %d", i);
 	printf("\n\n\ntempo de execucao: %d",tempo);
-	 	
-	return 0;
-	
 }
