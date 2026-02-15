95d8b60675f25798f89840a2eeec6ded242e778b
Bruno mendes <162513613+BrunoMendesMorais@users.noreply.github.com>
2026-02-15 18:09:16 -0300

    Organizando os arquivos por aula

---
+++ estrutura de dados/Aula1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+void main(int argc, char *argv[]) {
+	int i,tempo;
+	char h;
+	unsigned int valor, numeros[500];
+	clock_t c,f;
+	
+	for(i=0;i<500;i++){
+		numeros[i] = rand() % 500;
+ 	}
+ 	
+	printf("Qual valor deseja buscar?\n");
+	scanf("%u",&valor);
+	
+	c = clock();
+	for(i=0;i<500 && numeros[i]!=valor;i++){
+		usleep(1000);
+ 	}
+ 	f = clock();
+ 	
+ 	tempo = f-c;
+ 	numeros[i]!=valor ? printf("numero nao encontrado"):printf("O numero que pediu esta na posicao: %d", i);
+	printf("\n\n\ntempo de execucao: %d",tempo);
+	scanf("%c",&h);
+}
