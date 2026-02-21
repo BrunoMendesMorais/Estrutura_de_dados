9e8d3a791a04a5752eb50d63af9854e9bf0b9f37
Bruno mendes <162513613+BrunoMendesMorais@users.noreply.github.com>
2026-02-21 17:57:46 -0300

    Código refatorado e inserção de uma função de busca binaria recursiva

--- estrutura de dados/Aula2/main.c
+++ estrutura de dados/Aula2/main.c
@@ -5,18 +5,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int achaposicao(int ultimo, int primeiro,unsigned int data,unsigned int V[]){
-	int meio = trunc((primeiro + ultimo) /2);
-	if(V[meio] == data)
-		return -1;
-	if(ultimo == primeiro && data < V[meio])
-		return meio;
-	if(ultimo == primeiro && data > V[meio])
-		return meio;
-	if(data < V[meio]){
-		achaposicao(meio,primeiro,data,V);	
+	while(ultimo >= primeiro){
+		int meio = (primeiro + ultimo)/2;
+		if(V[meio] == data)
+			return -1;
+		data < V[meio] ? (ultimo = meio-1):(primeiro = meio +1);
 	}
-	else{
-		achaposicao(ultimo,meio+1,data,V);	
+	return primeiro;
+}
+
+void definirAutomatico(int tamanho,unsigned int numeros[]){
+	int posicao,i,valor,resposta;
+	for(i = 0; i < tamanho; i++){
+		valor = rand();
+		posicao = achaposicao(i-1,0,valor,numeros);
+		resposta = add(posicao,numeros,valor,i);
+		i = i + resposta;
 	}
 }
 
@@ -34,30 +38,34 @@ int add(int posicao,unsigned int V[], unsigned int data,int fim){
 	return 0;
 }
 
+int BinariaRec(unsigned int v[],unsigned int val, int esq, int dir){
+	int meio = (esq + dir)/2;
+	if((v[meio]==val) || (esq>dir))
+		return meio;
+	v[meio]< val?(BinariaRec(v,val,meio+1,dir)):(BinariaRec(v,val,esq,meio-1));
+}
+
 int main(int argc, char *argv[]) {
+	int tamanho, i;
+	unsigned int val;
 	
-	int tamanho,i,ultPos,posicao,resposta;
-	
-	printf("Tamho do vetor:");
+	printf("Tamanho do vetor:");
 	scanf("%d",&tamanho);
 	
-	unsigned int valor, numeros[tamanho];
+	unsigned numeros[tamanho];
 	
 	for(i = 0; i < tamanho; i++){
 		numeros[i] = 0;
 	}
+	definirAutomatico(tamanho,numeros);
 	
+	printf("Valor deseja procurar:");
+	scanf("%u",&val);
+	i = BinariaRec(numeros,val,0,tamanho-1);
 	
-	numeros[0] = rand;
-	
-	for(i = 1; i < tamanho; i++){
-		valor = rand();
-		posicao = achaposicao(i-1,0,valor,numeros);
-		resposta = add(posicao,numeros,valor,i);
-		i = i + resposta;
-	}
-	for(i = 0; i < tamanho-1; i++){
-		printf("\n%u",numeros[i]);
-	}
+	if(numeros[i] == val)
+		printf("o %u foi encontrado na posicao %d",val,i);
+	else
+		printf("valor nao encontrado");
 	return 0;
 }
